7cb118364b2a3c477b64c050052a0ac929dee1d3
Eugene Kuzora <Yauhen_Kuzora@epam.com>
2019-04-11 00:51:43 -0700

    init

---
+++ src/main/java/by/bsuir/http/client/HttpClient.java
@@ -0,0 +1,4 @@
+package by.bsuir.http.client;
+
+public class HttpClient {
+}
---
+++ src/main/java/by/bsuir/main/Main.java
@@ -0,0 +1,8 @@
+package by.bsuir.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
