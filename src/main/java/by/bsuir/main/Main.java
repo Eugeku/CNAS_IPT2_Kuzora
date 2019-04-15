bf544e3df5ab1be79b5baf35f8fbd995482aa5d2
Eugene Kuzora <Yauhen_Kuzora@epam.com>
2019-04-15 04:26:28 -0700

    update

--- src/main/java/by/bsuir/main/Main.java
+++ src/main/java/by/bsuir/main/Main.java
@@ -1,8 +1,15 @@
 package by.bsuir.main;
 
+import by.bsuir.http.client.HttpClient;
+import by.bsuir.http.client.enums.MethodValues;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        HttpClient httpClient = new HttpClient();
+        httpClient.createHttpConnection("https://httpbin.org/put", MethodValues.PUT);
+        httpClient.settingTimeOuts();
+        httpClient.settingRequestHeaders();
+        System.out.println(httpClient.getFullResponse());
     }
 }
