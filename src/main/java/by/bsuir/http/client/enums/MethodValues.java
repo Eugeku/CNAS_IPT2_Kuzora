cdcae071b6668833d4d8b16cc683504d9090b405
Eugene Kuzora <Yauhen_Kuzora@epam.com>
2019-04-15 04:27:16 -0700

    update

---
+++ src/main/java/by/bsuir/http/client/enums/MethodValues.java
@@ -0,0 +1,20 @@
+package by.bsuir.http.client.enums;
+
+public enum MethodValues {
+    GET("GET"),
+    POST("POST"),
+    HEAD("HEAD"),
+    OPTIONS("OPTIONS"),
+    PUT("PUT"),
+    DELETE("DELETE");
+
+    String value;
+
+    MethodValues(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
